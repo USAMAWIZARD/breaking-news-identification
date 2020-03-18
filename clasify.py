265fbbd56e6a79ab806a00a92465c6d714b9a940
USAMA <47350008+USAMAWIZARD@users.noreply.github.com>
2020-03-18 12:36:19 +0530

    most repeated word from all the news article identified

--- clasify.py
+++ clasify.py
@@ -2,6 +2,7 @@ import feedparser
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+mostrep={}
 tokenizer = RegexpTokenizer(r'\w+')
 titleofallnews=[]
 stop_words = set(stopwords.words('english')) 
@@ -11,13 +12,20 @@ cnn=feedparser.parse("http://rss.cnn.com/rss/cnn_education.rss")
 cnbcworld=feedparser.parse("https://www.cnbc.com/id/100727362/device/rss/rss.html")
 cnbctop= feedparser.parse("https://www.cnbc.com/id/100003114/device/rss/rss.html")
   
-
+def mostcommonwordnews():
+	global titleofallnews
+	for newsheadline in titleofallnews:
+		for word in newsheadline:
+			mostrep[word]+=1
+	print(mostrep) 
   
 for news in aljazeera.entries+bbc.entries+cnn.entries+cnbcworld.entries+cnbctop.entries	:
 
 	wordtokenized=word_tokenize(news.title)
 	stop_word_removed_sentence = [w for w in wordtokenized if not w in stop_words]
-	sentence_without_puct=tokenizer.tokenize(stop_word_removed_sentence)  
-	titleofallnews.append(stop_word_removed_sentence)
-
-print(titleofallnews)
+	sentence_without_puct=tokenizer.tokenize(str(stop_word_removed_sentence)  )
+	for word in sentence_without_puct:
+		mostrep[word]=0
+	titleofallnews.append(sentence_without_puct)
+mostcommonwordnews()
+#print(titleofallnews)
